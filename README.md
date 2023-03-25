83cfde9477e45cfdb12a7465637be396c7f57c49
tom <thomas16441@gmail.com>
2023-03-25 15:17:14 +1100

    Add all the classes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Human Detection using YOLOv3
+# Object Detection using YOLOv3
 
-This script uses the YOLOv3 object detection model to detect humans in a video stream. It can be used with any video stream, as long as the URL of the stream is specified in the script.
+This script uses the YOLOv3 object detection model to detect objects in a video stream. It can be used with any video stream, as long as the URL of the stream is specified in the script.
 
 ## Requirements
 
@@ -11,22 +11,39 @@ To run this script, you will need to have the following dependencies installed:
 
 You can install these dependencies using pip:
 
-`pip install opencv-python numpy`
+```pip install opencv-python numpy```
 
+
+You will also need to download the YOLOv3 configuration file and weights from the official YOLO website:
+
+- [yolov3.cfg](https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg)
+- [yolov3.weights](https://pjreddie.com/media/files/yolov3.weights)
+
+Make sure that the configuration file and weights are located in the same directory as your Python script.
 
 ## Usage
 
-To use this script, you will need to modify the `url` variable in the script to match the URL of the video stream you want to use.
+Before running the script, you will need to start a video stream using `ffmpeg`. You can use the following command to start a video stream from a webcam and stream it to a UDP address:
 
-Once you have modified the `url` variable, you can run the script using the following command:
-
-`python main.py`
+``ffmpeg -f v4l2 -i /dev/video0 -preset ultrafast -tune zerolatency -f mpegts udp://192.168.5.185:1234``
 
 
-This will open the video stream and start detecting humans in real-time. To exit the script, press the 'q' key.
+This command starts a video stream from `/dev/video0` (your webcam) and streams it to `udp://192.168.5.185:1234`.
+
+To use this script, you will need to modify the `url` variable in the script to match the URL of the video stream you want to use. In this case, the URL should be:
+
+``url = 'udp://192.168.5.185:1234'``
+
+
+You can also modify the `conf_threshold` and `iou_threshold` variables to adjust the detection sensitivity and non-maximum suppression overlap threshold, respectively.
+
+Once you have modified the variables, you can run the script using the following command:
+
+``python main.py``
+
+
+This will open the video stream and start detecting objects in real-time. Detected objects will be highlighted with bounding boxes and labeled with their class and confidence score. To exit the script, press the 'q' key.
 
 ## Acknowledgements
 
-This script uses the pre-trained YOLOv3 model, which was developed by the Darknet team. The original YOLOv3 paper can be found [here](https://arxiv.org/abs/1804.02767). You can download the YOLOv3 weights from [here](https://pjreddie.com/media/files/yolov3.weights).
-
-
+This script uses the pre-trained YOLOv3 model, which was developed by the Darknet team. The original YOLOv3 paper can be found [here](https://arxiv.org/abs/1804.02767).
--- main.py
+++ main.py
@@ -9,11 +9,18 @@ model = cv2.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
 model.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 model.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-# Define the minimum confidence threshold for human detection
+# Define the minimum confidence threshold for detection
 conf_threshold = 0.5
 
 # Define the maximum overlap threshold for non-maximum suppression
 iou_threshold = 0.4
+
+# Define the labels for the classes
+with open('coco.names', 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Define the colors for the bounding boxes
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
 # Open the video stream
 cap = cv2.VideoCapture(url)
@@ -33,47 +40,49 @@ while True:
         print("Failed to read a frame from the stream")
         break
 
-    # Detect humans in the frame using the YOLOv3 model
+    # Detect objects in the frame using the YOLOv3 model
     blob = cv2.dnn.blobFromImage(frame, 1/255.0, (128, 128), swapRB=True, crop=False)
     model.setInput(blob)
     output_layers = model.getUnconnectedOutLayersNames()
     layer_outputs = model.forward(output_layers)
 
-    # Process the model outputs to extract human detections
-    humans = []
+    # Process the model outputs to extract detections
+    detections = []
     for output in layer_outputs:
         for detection in output:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if class_id == 0 and confidence > conf_threshold:
+            if confidence > conf_threshold:
                 center_x = int(detection[0] * frame.shape[1])
                 center_y = int(detection[1] * frame.shape[0])
                 width = int(detection[2] * frame.shape[1])
                 height = int(detection[3] * frame.shape[0])
                 x = center_x - width//2
                 y = center_y - height//2
-                humans.append([x, y, width, height, confidence])
+                detections.append([x, y, width, height, confidence, class_id])
 
     # Apply non-maximum suppression to remove overlapping bounding boxes
-    if len(humans) > 0:
-        boxes = np.array(humans)[:, :4]
-        confidences = np.array(humans)[:, 4]
+    if len(detections) > 0:
+        boxes = np.array(detections)[:, :4]
+        confidences = np.array(detections)[:, 4]
+        class_ids = np.array(detections)[:, 5]
         indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, iou_threshold)
-        humans = [humans[i] for i in indices.flatten()]
+        detections = [detections[i] for i in indices.flatten()]
 
-    # Draw the human detections on the frame
-    for x, y, w, h, conf in humans:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.putText(frame, "Human", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+    # Draw the detections on the frame
+    for x, y, w, h, conf, class_id in detections:
+        label = f'{classes[class_id]}: {conf:.2f}'
+        color = colors[class_id]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     # Show the frame in a window
     cv2.imshow('Video Stream', frame)
 
-    # Wait for a key event and check if 'q' was pressed to exit
+# Wait for a key event and check if 'q' was pressed to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video stream and close the window
 cap.release()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
